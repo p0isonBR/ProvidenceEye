e78de5b9ed1d9f3bee47965baf5b35c5167f81f5
p0ison <65384918+p0isonBR@users.noreply.github.com>
2021-02-03 00:22:06 -0300

    Update start_menu.py
    Retirado lista "comandos em desenvolvimento", deixando apenas os disponiveis.

--- start_menu.py
+++ start_menu.py
@@ -13,20 +13,4 @@ _Comandos disponiveis:_
 */proxyssl*:  `Lista de proxies HTTPs`
 */proxy*: `Lista de proxies HTTP`
 */virustotal*: `Analisa um arquivo, a procura de virus`
-*/id*: `O bot ira identificalo`
-
-_Comandos em desenvolvimento:_
-*/socks4*: `Lista de proxies socks4`
-*/cnpj*: `Consulta completa de CNPJ`
-*/cep*: `Consulta de CEP`
-*/leak*: `Consulta senha de email (caso vazado)`
-*/instapics*: `Baixa as fotos postadas de um usuario (perfil publico)`
-*/twitter*: `Informacoes de um usuario do twitter`
-*/username*: `Analisa em mais de 100 redes sociais, se o username ja foi usado`
-*/gerarcc*: `Gera cartao de credito com o Bin fornecido`
-*/base64*: `Base64 encode`
-*/base64d*: `Base64 decode`
-*/md5*: `MD5 encode`
-*/md5d*: `MD5 decode (consulta banco de dados)`
-
-*Criador*: @p0isonBR"""
+*/id*: `O bot ira identificalo`"""
