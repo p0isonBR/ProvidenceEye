e4becb9a16509f1c0928ee4ac218d6990a338701
p0ison <65384918+p0isonBR@users.noreply.github.com>
2021-02-03 00:16:07 -0300

    Update id_user.py
    Retirado frase personalizada ao meu user id.

--- id_user.py
+++ id_user.py
@@ -1,6 +1,3 @@
 def UserID(message):
 
-    if message.from_user.id == 1054600955:
-        return '`Ola mestre supremo do universo`'
-    else:
-        return f'*Nome*: `{message.from_user.first_name}`\n*Username*: `{message.from_user.username}`\n*ChatID*: `{message.from_user.id}`'
+    return f'*Nome*: `{message.from_user.first_name}`\n*Username*: `{message.from_user.username}`\n*ChatID*: `{message.from_user.id}`'
