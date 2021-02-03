6c29f7bca4cec45c2b584ed7f17c152b22a0ed51
p0ison <65384918+p0isonBR@users.noreply.github.com>
2021-02-03 00:12:52 -0300

    Update virustotal.py
    Criado variavel para o Token do bot

--- virustotal.py
+++ virustotal.py
@@ -1,16 +1,17 @@
 import requests
 
 def VirusTotal(message):
+    bot_token = ''
     arq = message.json
     arq = arq['reply_to_message']['document']['file_id']
-    arq = requests.get(f'https://api.telegram.org/bot1414183645:AAH-jBkyE3Pemwf9EJCQt1w-MVNc45XUy9c/getFile?file_id={arq}').json()
+    arq = requests.get(f'https://api.telegram.org/bot{bot_token}/getFile?file_id={arq}').json()
     f = arq['result']['file_path']
-    arq = 'https://api.telegram.org/file/bot1414183645:AAH-jBkyE3Pemwf9EJCQt1w-MVNc45XUy9c/' + f
+    arq = f'https://api.telegram.org/file/bot{bot_token}/' + f
     with open(f.split('/')[1], 'wb') as file:
         file.write(requests.get(arq).content)
         file.close()
 
-    params = {'apikey': 'd1da02ce15b0aea70477b0929a67528e0f4f18f11bcb76bcf43760331bb59699'}
+    params = {'apikey': 'd1da02ce15b0aea70477b0929a67528e0f4f18f11bcb76bcf43760331bb59699'} #Deixei minha API Key do Virus Total, porem recomendo criar uma propria, pelos limites impostos a APIs Free.
     files = {'file': (f.split('/')[1], open(f.split('/')[1], 'rb'))}
     r = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params).json()
     if r['response_code'] == 1:
