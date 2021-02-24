b62a67d6df495f6cbcbcdc7c4e91b261b0011b58
p0isonBR <yuriavtomat@gmail.com>
2021-02-24 01:19:15 -0300

    Fixado o servidor de consultaCPF

--- consultacpf.py
+++ consultacpf.py
@@ -1,4 +1,5 @@
 from requests import get
+import re
 
 def ConsultaCPF(message):
 
@@ -8,17 +9,18 @@ def ConsultaCPF(message):
     elif message.text == '/cpfull@ProvidenceEye_Bot':
         return '`Insira o CPF apos o comando!`'
     else:
-        r = get(f'http://104.41.5.41:12345/cpf.php?lista={cpf}').json()
-        if r['Type'] == "PESSOAFISICA":
+        r = get(f'http://191.235.103.105/cpf/api.php?lista={cpf}').text
+        if "PESSOAFISICA" in r:
             return f'''
-*CPF*: `{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}`
-*Nome*: `{r["Nome"].title()}`
-*Nascimento*: `{r["Nacimento"]}`
-*Nome da Mae*: `{r["Mae"].title()}`
-*Endereco*: `{r["Nologradouro"].title()}, {r["Nrlogradouro"]}`
-*Complemento*: `{r["Complemento"].title()}`
-*Bairro*: `{r["Bairro"].title()}`
-*Cidade*: `{r["Municipio"].title()}-{r["Estado"]}`
-*CEP*: `{r["Cep"][:5]}-{r["Cep"][5:]}`'''
+*CPF*: `{re.search('NRCPF="(.*?)"', r).group(1)}`
+*Nome*: `{re.search('NOPESSOAFISICA="(.*?)"', r).group(1).title()}`
+*Nascimento*: `{re.search('DTNASCIMENTO="(.*?)"', r).group(1)}`
+*Nome da Mae*: `{re.search('NOMAE="(.*?)"', r).group(1).title()}`
+*Endereco*: `{re.search('NOLOGRADOURO="(.*?)"', r).group(1).title()}, {re.search('NRLOGRADOURO="(.*?)"', r).group(1)}`
+*Complemento*: `{re.search('DSCOMPLEMENTO="(.*?)"', r).group(1).title()}`
+*Bairro*: `{re.search('NOBAIRRO="(.*?)"', r).group(1).title()}`
+*Cidade*: `{re.search('NOMUNICIPIO="(.*?)"', r).group(1).title()}-{re.search('SGUF="(.*?)"', r).group(1)}`
+*CEP*: `{re.search('NRCEP="(.*?)"', r).group(1)}`
+''')
         else:
             return '`Documento nao encontrado!`'
