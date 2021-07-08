ea71d24b95205631cc6a960ababa02ef62b2df92
p0ison <poisonbr@protonmail.com>
2021-07-08 00:11:22 -0300

    Update start_menu.py

--- start_menu.py
+++ start_menu.py
@@ -2,14 +2,13 @@ def menu():
     return """*Digite o comando desejado a partir da lista de comandos*:
 
 _Comandos disponiveis:_
-*/cpfull*: `Consulta completa de CPF`
+*/cpfull*: `Consulta da situacao cadastral de CPF`
 */insta*: `Informacoes de um usuario do instagram`
 */ip*: `Informacoes sobre IP`
 */scan*: `Scanner de portas TCP de um IP`
 */bin*: `Consulta de BIN`
 */placa*: `Consulta placa de veiculo`
 */gerarcpf*: `Gerar CPF valido`
-*/gerarpessoa*: `Gera pessoa completa aleatoria`
 */proxyssl*:  `Lista de proxies HTTPs`
 */proxy*: `Lista de proxies HTTP`
 */virustotal*: `Analisa um arquivo, a procura de virus`
